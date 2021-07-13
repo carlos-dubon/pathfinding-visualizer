beff301b4544b6bd47fca889eb226217c5e9873e
carlos-dubon <holacarlosdubon@gmail.com>
2021-07-13 09:31:40 -0600

    🎲 Initial board markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pathfinding-visualizer';
+  public rows: number = 16;
+  public cols: number = 45;
+
+  constructor() {}
+
+  public ngOnInit(): void {}
 }
