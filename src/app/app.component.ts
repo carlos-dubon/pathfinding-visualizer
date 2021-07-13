1820357256c286b618c5e88db8ea0eed4285d87d
carlos-dubon <holacarlosdubon@gmail.com>
2021-07-13 10:41:14 -0600

    📐 Board generation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { createBoard, Tile } from './models/Board';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,33 @@ export class AppComponent {
   public rows: number = 16;
   public cols: number = 45;
 
+  public board: Array<Array<Tile>>;
+
+  @ViewChild('boardEl') boardEl: ElementRef<HTMLElement>;
+
   constructor() {}
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.board = createBoard(this.rows, this.cols);
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.board[i][j] = new Tile();
+      }
+    }
+  }
+
+  public ngAfterViewInit(): void {
+    let rowsCollection: HTMLCollection = this.boardEl.nativeElement.children;
+
+    for (let i = 0; i < this.rows; i++) {
+      let currentRow: Element = rowsCollection[i];
+
+      let colsCollection: HTMLCollection = currentRow.children;
+
+      for (let j = 0; j < this.cols; j++) {
+        let currentCol: Element = colsCollection[j];
+        this.board[i][j].DOMElement = currentCol;
+      }
+    }
+  }
 }
